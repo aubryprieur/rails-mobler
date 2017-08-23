6bf646e6a6d762b54e6a2644d2e31f263519bb53
guillaume trupin <gtrupin@gmail.com>
2017-08-23 14:31:21 +0200

    Voir son profil courant et ses informations

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,12 @@ class ProfilesController < ApplicationController
 
   def profile
     @wishlists = current_user.wishlists
+    @FirstName = current_user.first_name
+    @LastName = current_user.last_name
+    @avatar = current_user.photo
+    @email = current_user.email
     authorize :profile
   end
+
+
 end
