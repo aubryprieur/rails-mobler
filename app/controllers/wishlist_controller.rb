f8d6338fec52036b2d2cd689d6311abdda17e91a
guillaume trupin <gtrupin@gmail.com>
2017-08-24 12:19:27 +0200

    Je peux voir les meubles associées à mes wishlists. Je peux supprimer mes wishlists dans mon profil ou dans une wishlist.

--- app/controllers/wishlist_controller.rb
+++ app/controllers/wishlist_controller.rb
@@ -2,13 +2,22 @@ class WishlistController < ApplicationController
 
   def show
     @wishlist = Wishlist.find(params[:id])
+    @furnitures = @wishlist.furnitures
     authorize @wishlist
   end
 
-#A configurer en récupérant les infos de la modale popup dans l'index de furnitures
-  def create
-    @wishlist = Wishlist.new
-    @wishlist.user = current_user
-    @wishlist.save
+  def destroy
+    @wishlist = Wishlist.find(params[:id])
+    @wishlist.destroy
+    authorize @wishlist
+    redirect_to profile_path
+
   end
+
+#A configurer en récupérant les infos de la modale popup dans l'index de furnitures
+  # def create
+  #   @wishlist = Wishlist.new
+  #   @wishlist.user = current_user
+  #   @wishlist.save
+  # end
 end
