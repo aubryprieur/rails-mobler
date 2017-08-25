36b13332a6ba44adc6d8dc61e6f4034fcb6d822e
Bertrand Matelart <matelart.bertrand@gmail.com>
2017-08-25 15:02:54 +0200

    in progress

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get '/profile' => 'profiles#profile'
-  # resources :wishlist, only: [ :index, :show, :create, :update, :destroy ] do
-  # resources :furnitures_wishlist, only: [ :create, :destroy ] do
-  # resources :reviews, only: [ :create, :destroy ]
-  #   end
-  # end
-  resources :wishlist, only: [:show, :destroy]
+
+  resources :wishlist, only: [:show, :destroy] do
+    resources :wishlist_items, only: [:create]
+  end
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
