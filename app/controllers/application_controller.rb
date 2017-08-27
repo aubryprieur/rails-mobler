a88f79b380ea1849832ba2849b43804ea772f135
Bertrand Matelart <matelart.bertrand@gmail.com>
2017-08-27 18:46:59 +0200

    add redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   after_action :return_errors, only: [:page_not_found, :server_error]
 
+
+  # saves the location before loading each page so we can return to the
+  # right page. If we're on a devise page, we don't want to store that as the
+  # place to return to (for example, we don't want to return to the sign in page
+  # after signing in), which is what the :unless prevents
+  before_action :store_current_location, :unless => :devise_controller?
+
   include Pundit
 
   # Pundit: white-list approach.
@@ -18,6 +25,21 @@ class ApplicationController < ActionController::Base
 
 
   private
+
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+  # override the devise method for where to go after signing out because theirs
+  # always goes to the root path. Because devise uses a session variable and
+  # the session is destroyed on log out, we need to use request.referrer
+  # root_path is there as a backup
+  def after_sign_out_path_for(resource)
+    request.referrer || root_path
+  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
