9c71dc7fbea8fc3d0113372f119fc5c6bb5ed194
Bertrand Matelart <matelart.bertrand@gmail.com>
2017-08-29 16:20:09 +0200

    search params to session ok

--- app/controllers/furnitures_controller.rb
+++ app/controllers/furnitures_controller.rb
@@ -1,26 +1,24 @@
 class FurnituresController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index, :show]
+  skip_before_action :authenticate_user!, only: [ :index, :show, :destroy]
+
 
   def index
     @categories = Category.all
     @wishlist = Wishlist.new
-    return @furnitures = policy_scope(Furniture).page(params[:page]) unless params[:search].present?
 
-    if params.fetch(:search, {})[:category].present?
-      @furnitures = policy_scope(Furniture).where(category: params[:search][:category],
-                                                  width: (params[:search][:min_width].present? ? params[:search][:min_width].to_i : 0)..(params[:search][:max_width].present? ? params[:search][:max_width].to_i : 400),
-                                                  height: (params[:search][:min_height].present? ? params[:search][:min_height].to_i : 0)..(params[:search][:max_height].present? ? params[:search][:max_height].to_i : 400),
-                                                  length: (params[:search][:min_length].present? ? params[:search][:min_length].to_i : 0)..(params[:search][:max_length].present? ? params[:search][:max_length].to_i : 400)
-                                                  ).page(params[:page]).order("price")
-    else
-      @furnitures = policy_scope(Furniture).where(width: (params[:search][:min_width].present? ? params[:search][:min_width].to_i : 0)..(params[:search][:max_width].present? ? params[:search][:max_width].to_i : 400),
-                                                  height: (params[:search][:min_height].present? ? params[:search][:min_height].to_i : 0)..(params[:search][:max_height].present? ? params[:search][:max_height].to_i : 400),
-                                                  length: (params[:search][:min_length].present? ? params[:search][:min_length].to_i : 0)..(params[:search][:max_length].present? ? params[:search][:max_length].to_i : 400)
-                                                  ).page(params[:page]).order("price")
-    end
+    session[:search] = params[:search] if params[:search].present?
+    request = policy_scope(Furniture)
+
+    return @furnitures = request.page(params[:page]) unless session[:search].present?
+
+    request = request.where(category: session[:search]['category']) if session[:search]['category'].present?
+    request = request.where(
+      width: (session[:search]['min_width'].present? ? session[:search]['min_width'].to_i : 0)..(session[:search]['max_width'].present? ? session[:search]['max_width'].to_i : 400),
+      height: (session[:search]['min_height'].present? ? session[:search]['min_height'].to_i : 0)..(session[:search]['max_height'].present? ? session[:search]['max_height'].to_i : 400),
+      length: (session[:search]['min_length'].present? ? session[:search]['min_length'].to_i : 0)..(session[:search]['max_length'].present? ? session[:search]['max_length'].to_i : 400)
+    )
+    @furnitures = request.page(params[:page])
   end
-
-
 
   def show
     @wishlist = Wishlist.new
